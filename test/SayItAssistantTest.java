1e3ddfaac397ce31dc31b6f31e96c6efe5aea9bf
Andrew <Cubified@users.noreply.github.com>
2023-05-05 10:19:29 -0700

    Add testing and linting, general cleanups

---
+++ test/SayItAssistantTest.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SayItAssistantTest {
+  @Test
+  void testDummy() {
+    System.out.println("Not implemented.");
+    assertEquals(0, 0);
+  }
+}
