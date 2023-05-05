1445f4da0a575fd69fc7461757a61203c59541d6
Andrew <Cubified@users.noreply.github.com>
2023-05-05 10:39:08 -0700

    #10: Add linter to CI/CD

--- test/SayItAssistantTest.java
+++ test/SayItAssistantTest.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SayItAssistantTest {
   @Test
   void testDummy() {
-    System.out.println("Not implemented.");
     assertEquals(0, 0);
   }
 }
