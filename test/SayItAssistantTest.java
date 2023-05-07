11d4b3a03582ed8bc92ed0137cf11970bc68092f
adhi0331 <adhi03.ananthan@gmail.com>
2023-05-07 16:50:55 -0700

    added to make sure that audio is being recorderd

--- test/SayItAssistantTest.java
+++ test/SayItAssistantTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Top-level test runner, verifying general program behavior.
@@ -12,5 +13,27 @@ public class SayItAssistantTest {
   @Test
   void testDummy() {
     assertEquals(0, 0);
+  }
+
+  /**
+   * Tests to ensure that a recording file is generated
+   */
+  @Test
+  void testAudioRecord() {
+    AudioRecorder recorder = new AudioRecorder();
+
+    recorder.startRecording();
+
+    try {
+      Thread.sleep(5000);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    recorder.stopRecording();
+
+    File file = new File("question.wav");
+
+    assertTrue(file.exists());
+
   }
 }
